9682dfe63c36bfeec0f1339eae1fa7ff40251186
tommycarale <Tommy.carale12@gmail.com>
2024-08-27 19:19:44 +0800

    Updated

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,19 @@
-public class Dateable {
+import java.util.Scanner;
 
+public class Dateable {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your age: ");
+        int age = scanner.nextInt();
+        String name = scanner.nextLine();
+        System.out.println("You should date someone who is at least "+(7+age/2));
+      
+
+
+
+
+
+
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+                System.out.print("please enter age:");
+                int age = scanner.nextInt();
+              String name = scanner.nextLine();
+              System.out.println( age + " year olds must read " + (100-age) + " pages before giving up on a book");
+
+
+
+
+
 
     }
 }
